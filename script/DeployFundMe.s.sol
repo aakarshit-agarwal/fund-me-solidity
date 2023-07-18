a5a2e6e46e299cfe1a0da9f4339b915f0546c9b6
Aakarshit Agarwal <aakarshit.agarwal@gmail.com>
2023-07-18 20:06:31 +0530

    Deploy script for FundMe contract

---
+++ script/DeployFundMe.s.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import {Script} from "forge-std/Script.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract DeployFundMe is Script {
+    function run() public returns (FundMe) {
+        vm.startBroadcast();
+        FundMe fundMe = new FundMe();
+        vm.stopBroadcast();
+        return fundMe;
+    }
+}
