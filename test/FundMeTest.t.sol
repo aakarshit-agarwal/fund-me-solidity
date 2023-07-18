f41ca98fd74eb57f6b837de5745ef07bd679b3ce
Aakarshit Agarwal <aakarshit.agarwal@gmail.com>
2023-07-18 20:00:11 +0530

    Adding entry fee requirement test for FundMe

---
+++ test/FundMeTest.t.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundMeTest is Test {
+    FundMe fundMe;
+
+    function setUp() external {
+        fundMe = new FundMe();
+        console.log("This is test setup");
+    }
+
+    function testEntryFeeRequirement() public {
+        assertEq(fundMe.MINIMUM_USD(), 5 * 10 ** 18);
+        console.log("This is a test");
+    }
+}
